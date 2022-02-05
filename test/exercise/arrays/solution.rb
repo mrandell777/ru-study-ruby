caa34a67fb643fbabc109eada4491a9584e4356b
mrandell <zamagsvoishi@gmail.com>
2022-02-05 12:09:51 +0300

    Fix solution arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -3,7 +3,8 @@ module Exercise
     class << self
       def replace(array)
         max = array[0]
-        array.each { |el| el > max ? max = el : max }.map { |el| el.positive? ? max : el }
+        array.each { |el| el > max ? max = el : max }
+        array.map { |el| el.positive? ? max : el }
       end
 
       def search(array, query)
@@ -15,7 +16,7 @@ module Exercise
         elsif query < array[mid]
           search(array[0..mid - 1], query)
         elsif query > array[mid]
-          search(array[mid + 1..-1], query) + mid + 1
+          search(array[mid + 1..], query) + mid + 1
         end
       end
     end
